818a47c041e3db59f5079340a1747aa485612aea
jhanmtl <jing.han@mail.mcgill.ca>
2020-09-03 09:21:23 -0400

    Updates

---
+++ precache-manifest.9671b5f977562f313a3acb64c362f14d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbb0d95e1f1aea54090eee0f0aad39a5",
+    "url": "/eye-scroller/index.html"
+  },
+  {
+    "revision": "0c5547715ef4ca5dfb87",
+    "url": "/eye-scroller/static/css/main.d13a6b4f.chunk.css"
+  },
+  {
+    "revision": "50806189a6245c47546e",
+    "url": "/eye-scroller/static/js/2.738a12f3.chunk.js"
+  },
+  {
+    "revision": "f5ee1f3f67d5ae6ef79935301cdb748b",
+    "url": "/eye-scroller/static/js/2.738a12f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c5547715ef4ca5dfb87",
+    "url": "/eye-scroller/static/js/main.e8cc5537.chunk.js"
+  },
+  {
+    "revision": "5152ccd4145da06ea8a6",
+    "url": "/eye-scroller/static/js/runtime-main.11df09ae.js"
+  }
+]);
